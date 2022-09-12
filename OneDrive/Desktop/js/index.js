168d9c318d2dddb2d57f80c4a5be2ba9b27cefd2
AlanGolubeff <alangolu99@gmail.com>
2022-09-12 11:37:15 -0300

    primeros pasos con arrays

--- OneDrive/Desktop/js/index.js
+++ OneDrive/Desktop/js/index.js
@@ -1,4 +1,38 @@
-let total = 0
+class Producto {
+    constructor(id, nombre, precio, categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = parseInt(precio);
+        this.categoria = categoria;
+    }
+}
+const productosId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+const nombres = ["remera", "carlo", "campera", "pantalon", "zapatillas", "gorra", "buzo", "camisa", "pantaloneta", "remera", "campera", "pantalon", "zapatillas", "gorra", "buzo", "camisa", "pantaloneta", "remera", "campera", "pantalon"];
+
+const categoria = ["hombre", "mujer", "nino", "nina", "calzado", "accesorios"];
+
+const alala = [];
+
+for (let productoS of productosId) {
+    alala.push(new Producto(productosId[productoS], nombres[Math.floor(Math.random() * 20)], Math.floor(Math.random() * 1000), categoria[Math.floor(Math.random() * 6)]));
+}
+
+function elegir() {
+    let productoelegido = prompt("ingresar producto")
+    console.log(alala[parseInt(productoelegido)])
+}
+for (let index = 0; index < 5; index++) {
+    elegir();
+}
+
+
+
+
+
+
+
+/*let total = 0
 function calculo() {
     let producto = prompt('Ingresar Producto')
     let valor = parseFloat(prompt('Ingresar Precio'))
@@ -7,14 +41,13 @@ function calculo() {
     console.log(asignarProducto(producto, valor));
 }
 const iva = (precio) => { return precio * 1.21 }
-const asignarProducto = (producto, precio) => "producto" + producto + "precio base" + precio + ' precio con iva ' + iva(precio);
+const asignarProducto = (producto, precio) => " producto " + producto + " precio base " + precio + ' precio con iva ' + iva(precio);
 for (let index = 0; index < 5; index++) {
     calculo();
 }
 alert(' Tu Total es de ' + total);
 console.log(' Tu Total es de ' + total);
-
-
+*/
 
 
 
