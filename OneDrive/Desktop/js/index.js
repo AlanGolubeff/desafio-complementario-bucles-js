ded5af71f266378c5af3571b706837c80631b0bc
AlanGolubeff <alangolu99@gmail.com>
2022-09-28 21:23:52 -0300

    siguiendo con js

--- OneDrive/Desktop/js/index.js
+++ OneDrive/Desktop/js/index.js
@@ -7,6 +7,7 @@ class Articulo {
         this.imagen = imagen;
     }
 }
+let guardados = []
 
 
 const productosId = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
@@ -49,39 +50,51 @@ function init() {
 
 
     //Para ELBOTON descomentar desde linea 53 hasta linea 85 
-    /*
-        const elBoton = document.createElement("button"); // creo EL BOTON
-        divBoton.appendChild(elBoton); //adjunto EL BOTON a su div
-        elBoton.setAttribute('class', 'elBoton') //le asigno la clase a EL BOTON
-        elBoton.innerHTML = ("Apriete el boton"); //relleno el texto de EL BOTON
-        elBoton.addEventListener('click', destruccion) //asigno el evento a EL BOTON
-        function destruccion() {
-            pepe.innerHTML = ""; //lo vuelvo a vaciar para que destruya los divs y no se acumulen cada vez que apretamos EL BOTON
-            let elegir = prompt('elija un numero del 0 al 10') //pido un numero para crear random
-            let productito = productosAr[parseInt(elegir)]; //le paso a una variable el producto capturado
-            let elDiv = document.createElement('div') //creo un div contenedor
-            elDiv.setAttribute('class', 'elDiv')
-            pepe.appendChild(elDiv); //adjunto el contenedor al div pepe
-            let elNombre = document.createElement(`p`); //creo el tag para pasar el nombre
-            elNombre.setAttribute('class', 'elNombre'); //le asigno la clase al tag
-            elDiv.appendChild(elNombre);    //adjunto el tag 
-            let elPrecio = document.createElement(`p`);//creo el tag para pasar el nombre
-            elDiv.appendChild(elPrecio);    //adjunto el tag
-            elPrecio.setAttribute('class', 'elPrecio');     //le asigno la clase al tag
-            let laCategoria = document.createElement(`p`);//creo el tag para pasar el nombre
-            elDiv.appendChild(laCategoria);     //adjunto el tag
-            laCategoria.setAttribute('class', 'laCategoria');   //le asigno la clase al tag
-            let laImagen = document.createElement(`img`);   //creo el tag para pasar el nombre
-            laImagen.setAttribute('class', 'laImagen');     //le asigno la clase al tag
-            laImagen.setAttribute('src', `./img/${productito.imagen}.jpg`) //le asigno el atributo src para que me busque la imagen
-            elDiv.appendChild(laImagen);    //adjunto el tag
-    
-            //con template strings le paso los valores del producto capturado
-            elNombre.innerHTML = `Nombre: ${productito.nombre}`;
-            elPrecio.innerHTML = `Precio: $ ${productito.precio}`;
-            laCategoria.innerHTML = `Categoria: ${productito.categoria}`;
-        }
-    */
+
+    const elBoton = document.createElement("button"); // creo EL BOTON
+    divBoton.appendChild(elBoton); //adjunto EL BOTON a su div
+    elBoton.setAttribute('class', 'elBoton') //le asigno la clase a EL BOTON
+    elBoton.innerHTML = ("Apriete el boton"); //relleno el texto de EL BOTON
+    elBoton.addEventListener('click', destruccion) //asigno el evento a EL BOTON
+    function destruccion() {
+        pepe.innerHTML = ""; //lo vuelvo a vaciar para que destruya los divs y no se acumulen cada vez que apretamos EL BOTON
+        let elegir = prompt('elija un numero del 0 al 10') //pido un numero para crear random
+        let productito = productosAr[parseInt(elegir)]; //le paso a una variable el producto capturado
+        let elDiv = document.createElement('div') //creo un div contenedor
+        elDiv.setAttribute('class', 'elDiv')
+        pepe.appendChild(elDiv); //adjunto el contenedor al div pepe
+        let elNombre = document.createElement(`p`); //creo el tag para pasar el nombre
+        elNombre.setAttribute('class', 'elNombre'); //le asigno la clase al tag
+        elDiv.appendChild(elNombre);    //adjunto el tag 
+        let elPrecio = document.createElement(`p`);//creo el tag para pasar el nombre
+        elDiv.appendChild(elPrecio);    //adjunto el tag
+        elPrecio.setAttribute('class', 'elPrecio');     //le asigno la clase al tag
+        let laCategoria = document.createElement(`p`);//creo el tag para pasar el nombre
+        elDiv.appendChild(laCategoria);     //adjunto el tag
+        laCategoria.setAttribute('class', 'laCategoria');   //le asigno la clase al tag
+        let laImagen = document.createElement(`img`);   //creo el tag para pasar el nombre
+        laImagen.setAttribute('class', 'laImagen');     //le asigno la clase al tag
+        laImagen.setAttribute('src', `./img/${productito.imagen}.jpg`) //le asigno el atributo src para que me busque la imagen
+        elDiv.appendChild(laImagen);    //adjunto el tag
+
+        //con template strings le paso los valores del producto capturado
+        elNombre.innerHTML = `Nombre: ${productito.nombre}`;
+        elPrecio.innerHTML = `Precio: $ ${productito.precio}`;
+        laCategoria.innerHTML = `Categoria: ${productito.categoria}`;
+
+
+
+        let store = JSON.parse(localStorage.getItem("guardados"));
+        console.log(guardados)
+        guardados.log(productito)
+        localStorage.setItem("guardados", JSON.stringify(guardados));
+
+        let continuarALosSiguientes = document.createElement("a");
+        continuarALosSiguientes.innerHTML = "ver todos los productos elegidos";
+        continuarALosSiguientes.setAttribute("href", "final.html");
+        pepe.appendChild(continuarALosSiguientes)
+    }
+
 
     //Generador DOM Random descomentar linea 87 hasta linea 127
     /*
@@ -125,12 +138,6 @@ function init() {
         }
     */
 }
-
-
-
-
-
-
 
 
 
